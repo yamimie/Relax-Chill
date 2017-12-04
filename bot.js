2316ec870a5dce8927f86b462eae6feebb255fdd
yamimie <34231369+yamimie@users.noreply.github.com>
2017-12-04 16:19:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message',message => {
 });
 
 // THIS MUST BE THIS WAY
-client.login(process.env.Mzg3MTQ2NTc1MzEyNDUzNjMy.DQaVqw.WvzqfIbvZhEsto8tJWtq86WBPvU);
+client.login(process.env.BOT_TOKEN);
